2ade35576b07e4173468a989810584f719eb6de5
simon <simonfisher227@gmail.com>
2023-04-17 12:17:01 +0800

    complete task getGuess and passed the NaiveLetterFreqGuesser test

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -3,6 +3,8 @@ package aoa.guessers;
 import aoa.utils.FileUtils;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.Set;
 
 public class NaiveLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -20,15 +22,43 @@ public class NaiveLetterFreqGuesser implements Guesser {
     /** Returns a map from a given letter to its frequency across all words.
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
-        // TODO: Fill in this method.
-        return null;
+        Map<Character, Integer> resultMap = new TreeMap<>();
+        for(String str : words) {
+            int len = str.length();
+            for(int i=0; i<len; i++) {
+                char cha = str.charAt(i);
+                resultMap.merge(cha, 1, Integer::sum);
+            }
+        }
+        return resultMap;
     }
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
-        // TODO: Fill in this method.
-        return '?';
+        // create map contained entries except keys in guesses list
+        // guesses list is already alphabet sorted
+        Map<Character, Integer> freqMap = this.getFrequencyMap();
+        Map<Character, Integer> processedFreqMap = new TreeMap<>();
+        Set<Character> keys = freqMap.keySet();
+        guesses.forEach(keys::remove);
+        for(char key : keys) {
+            processedFreqMap.put(key, freqMap.get(key));
+        }
+
+        if(processedFreqMap.isEmpty()) {
+            return '?';
+        }
+
+        // find key associated with max value in processedFreqMap
+        Map.Entry<Character, Integer> maxEntry = null;
+        for (Map.Entry<Character, Integer> entry : processedFreqMap.entrySet()) {
+            if(maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0 ) {
+                maxEntry = entry;
+            }
+        }
+
+        return maxEntry.getKey();
     }
 
     public static void main(String[] args) {
