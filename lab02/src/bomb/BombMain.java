890ed6b0331692d00592b5c3bee8d2106f5eab0b
simon <simonfisher227@gmail.com>
2023-04-21 18:37:20 +0800

    bomb debugged, it is safe now

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -2,6 +2,10 @@ package bomb;
 
 import common.IntList;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class BombMain {
     public static void main(String[] args) {
         int phase = 2;
@@ -11,13 +15,26 @@ public class BombMain {
         // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+
+            IntList list = IntList.of(0,9,3,0,8);
+            assert list != null;
+            list.print();
+            b.phase1(list); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            Random r = new Random(1337);
+            Set<Integer> numbers = new HashSet<>();
+            while (numbers.size() < 100000) {
+                numbers.add(r.nextInt());
+            }
+            Object[] arr = numbers.toArray();
+            int targetI = (int) arr[1337];
+            String str = "p ".repeat(1337) + String.valueOf(targetI);
+
+            b.phase2(str);
         }
     }
 }
