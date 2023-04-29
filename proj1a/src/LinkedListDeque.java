43eb8de62129f52e1628f71cf5e100f0a08e3d75
simon <simonfisher227@gmail.com>
2023-04-30 00:46:24 +0800

    implemented addFirst and addLast of Deque

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -8,24 +8,49 @@ public class LinkedListDeque<T> implements Deque<T> {
         Node<T> prev;
         T item;
         Node<T> next;
-    }
-    public static void main(String[] args) {
-        Deque<Integer> lld = new LinkedListDeque<>();
+
+        public Node(T item) {
+            this.item = item;
+        }
+        public Node() {}
+        public Node(T item, Node<T> prev, Node<T> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 
     public LinkedListDeque() {
         sentinel = new Node<>();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         size = 0;
     }
 
     @Override
     public void addFirst(T x) {
+        Node<T> newNode = new Node<>(x, sentinel, sentinel.next);
 
+        if(size == 0) { // both first and last
+            sentinel.prev = newNode;
+        } else {
+            sentinel.next.prev = newNode;
+        }
+        sentinel.next = newNode;
+
+        size += 1;
     }
 
     @Override
     public void addLast(T x) {
-
+        if(size == 0) {
+            this.addFirst(x);
+        } else {
+            Node<T> newNode = new Node<>(x, sentinel.prev, sentinel);
+            sentinel.prev.next = newNode;
+            sentinel.prev = newNode;
+            size += 1;
+        }
     }
 
     @Override
@@ -61,5 +86,16 @@ public class LinkedListDeque<T> implements Deque<T> {
     @Override
     public T getRecursive(int index) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> lld = new LinkedListDeque<>();
+        lld.addLast(99);
+        lld.addFirst(1);
+        lld.addFirst(3);
+        lld.addFirst(5);
+        lld.addLast(88);
+        lld.addLast(77);
+        System.out.println(size);
     }
 }
